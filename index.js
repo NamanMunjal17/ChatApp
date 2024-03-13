ab12548bdb788f984effe685bf42cd548bdcac50
Naman Munjal <74naman@gmail.com>
2024-03-13 20:50:19 +0530

    almost there

--- index.js
+++ index.js
@@ -2,49 +2,100 @@ const { Server } = require("socket.io");
 const dotenv = require("dotenv");
 const { Redis } = require("ioredis");
 const { initializeApp } = require("firebase/app");
-const { getFirestore, doc, setDoc } = require("firebase/firestore");
+const { getFirestore, doc, setDoc, addDoc, updateDoc, arrayUnion, getDoc } = require("firebase/firestore");
 
-//TODO Store all this in dotenv file
+dotenv.config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAfkrihGesm4G6LlzAcxYrTUJmlB7dsWlI",
-    authDomain: "chat-app-4a7f6.firebaseapp.com",
-    projectId: "chat-app-4a7f6",
-    storageBucket: "chat-app-4a7f6.appspot.com",
-    messagingSenderId: "784317897889",
-    appId: "1:784317897889:web:0f24b5d2ced04d472524e0"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 
-dotenv.config()
 
-const io = new Server(3000, { /* options */ });
+const io = new Server(3000, {
+  cors: { origin: "*" },
+});
 
-const pub = new Redis({ host: process.env.HOST, port: process.env.PORT, username: process.env.USER, password: process.env.PASSWORD })
-const sub = new Redis({ host: process.env.HOST, port: process.env.PORT, username: process.env.USER, password: process.env.PASSWORD })
+const pub = new Redis({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+});
+const sub = new Redis({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+});
 
-const sockToId={};
+
+const sockToId = {};
+const idToSock={};
 
 io.on("connection", (socket) => {
+  console.log("Connection Recvd",socket.id)
+  socket.on("online", async ({profile}) => {
+    if (profile["email"]) {
+      sockToId[socket.id] = profile["email"];
+      await setDoc(doc(db, "users", profile["email"].split("@")[0]), {
+        isOnline: true,
+      });
+      sockToId[socket.id]=profile["email"].split("@")[0]
+      idToSock[profile["email"].split("@")[0]]=socket.id
+      docSnap=await getDoc(doc(db,"contactlist",profile["email"].split("@")[0]));
+      if(docSnap.exists()){
+        io.to(socket.id).emit("contactListSent",docSnap.data());
+      }
+    }
+  });
 
-    socket.on('online', async (id) => {
-        sockToId[socket.id]=id;
-        await setDoc(doc(db,"users",id),{
-            isOnline: true
-        })
-    })
+  socket.on("disconnect", async (id) => {
+    if(sockToId[socket.id]){
+      await setDoc(doc(db, "users", sockToId[socket.id]), {
+        isOnline: false,
+      });
+      delete sockToId[socket.id];
+    }
+    
+  });
 
-    socket.on('disconnect', async (id)=>{
-        await setDoc(doc(db,"users",sockToId[socket.id]),{
-            isOnline: false
-        })
-        delete sockToId[socket.id];
-    })
+  socket.on("addInContacts", async(id)=>{
+    console.log(id)
+    try{
+      await updateDoc(doc(db, "contactlist", id["ids"][0]), {
+        contacts: arrayUnion(id["ids"][1]),
+      });
+    }
+    catch{
+      await setDoc(doc(db, "contactlist", id["ids"][0]), {
+        contacts: [id["ids"][1]],
+      });
+    }
+  })
 
-    socket.on('message', async (message) => {
-        await pub.publish("messages", message)
-    })
+  socket.on("message", async (message) => {
+    await pub.publish("messages", message);
+  });
+
 
 });
+
+
+sub.subscribe('messages')
+sub.on('message',(channel,message)=>{
+  message=JSON.parse(message);
+  console.log(message)
+  console.log(idToSock)
+  if(message["to"] in idToSock){
+    io.to(idToSock[message["to"]]).emit("message",JSON.stringify({from:message["from"],msg:message["msg"]}))
+  }
+})
